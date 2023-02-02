74437a0c3740c090308fe688f9822635e29be9e2
isurusachinda85 <isurusachintha8@gmail.com>
2023-02-03 01:26:49 +0530

    Add method save customer in employee dao

--- src/lk/ijse/phoneshop/dao/EmployeeDAOImpl.java
+++ src/lk/ijse/phoneshop/dao/EmployeeDAOImpl.java
@@ -1,4 +1,14 @@
 package lk.ijse.phoneshop.dao;
 
+import lk.ijse.phoneshop.to.Employee;
+import lk.ijse.phoneshop.util.CrudUtil;
+
+import java.sql.SQLException;
+
 public class EmployeeDAOImpl {
+    public  boolean saveEmployee(Employee employee) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT Into employee values (?,?,?,?,?,?,?,?,?)";
+        return CrudUtil.execute(sql,employee.getId(),employee.getName(),employee.getAddress(),employee.getEmail(),
+                employee.getPhoneNo(),employee.getDateOfBirth(),employee.getJobRole(),employee.getUserName(),employee.getPassword());
+    }
 }
