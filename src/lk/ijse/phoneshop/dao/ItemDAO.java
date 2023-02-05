9f3cbac6ee93c755ef329d9e58ac33a202d559b1
isurusachinda85 <isurusachintha8@gmail.com>
2023-02-06 03:13:58 +0530

    Update

--- src/lk/ijse/phoneshop/dao/ItemDAO.java
+++ src/lk/ijse/phoneshop/dao/ItemDAO.java
@@ -1,4 +1,21 @@
 package lk.ijse.phoneshop.dao;
 
+import lk.ijse.phoneshop.db.DBConnection;
+import lk.ijse.phoneshop.to.Item;
+import lk.ijse.phoneshop.util.CrudUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public interface ItemDAO {
+    boolean itemAdd(Item item) throws SQLException, ClassNotFoundException;
+    ArrayList<Item> loadAllPhone() throws SQLException, ClassNotFoundException;
+    ArrayList<Item> loadAccessories() throws SQLException, ClassNotFoundException;
+    ArrayList<Item> loadParts() throws SQLException, ClassNotFoundException;
+    boolean deleteItem(String code) throws SQLException, ClassNotFoundException;
+    Item searchItem(String code) throws SQLException, ClassNotFoundException;
+    ResultSet loadItemCode() throws SQLException, ClassNotFoundException;
 }
